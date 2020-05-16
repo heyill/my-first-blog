030838e662badb739c7bfd4bd9fff411d33bd793
Heil <eragon.park.4@gmail.com>
2020-05-16 11:08:33 +0100

    My Django Girls app, first commit

---
+++ practiceone.py
@@ -0,0 +1,15 @@
+name = 'Sonja'
+if name == 'Ola':
+    print('Hey Ola!')
+elif name == 'Sonja':
+    print('Hey Sonja!')
+else:
+    print('Hey anonymous!')
+
+def hi(name):
+    print('Hi ' + name + '!')
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+for name in girls:
+    hi(name)
+    print('Next girl')
