9606bed6e2ff5b6968897407ba89694cd883af27
Heil <eragon.park.4@gmail.com>
2020-06-28 18:26:52 +0100

    Changed CSS and added a new view for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    re_path(r'^post/\d+/about/$', views.about, name='site_about'),
+    re_path(r'^post/\d+/edit/about/$', views.about, name='site_about'),
+    path('post/new/about', views.about, name='site_about'),
+    path('about', views.about, name='site_about'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
--- blog/views.py
+++ blog/views.py
@@ -13,6 +13,9 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+def about(request):
+    return render(request, 'blog/about.html')
+
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -38,4 +41,4 @@ def post_edit(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})    
+    return render(request, 'blog/post_edit.html', {'form': form})
